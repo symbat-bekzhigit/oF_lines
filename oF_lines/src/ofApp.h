b8c2354e7e18b9a2b413a6300998e7f73aa51892
Symbat Bekzhigit <sb7971@nyu.edu>
2022-04-04 15:24:55 +0400

    Create ofApp.h

---
+++ oF_lines/src/ofApp.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGui.h"
+#include "Circle.hpp"//allows access circle class
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    //ofPolyline myLine;
+    //-------------------
+//    ofxPanel gui;
+    
+//    ofxIntSlider intSlider;
+//    ofxFloatSlider floatSlider;
+//
+//    ofxToggle toggle;//toggles between True and False
+//    ofxButton button;
+//    ofxLabel label;
+//
+//    //allows user to type int, float, text values instead of using a slider
+//    ofxIntField intField;
+//    ofxFloatField floatField;
+//    ofxTextField textField;
+//
+//    //vector sliders
+//    ofxVec2Slider vec2Slider;
+//    ofxVec3Slider vec3Slider;
+//    ofxVec4Slider vec4Slider;
+    
+   //-------------------
+    ofxPanel gui;
+    ofParameterGroup sliderGroup;
+    ofParameter<int> intSlider;
+    ofParameter<float> floatSlider;
+    
+    Circle circle;
+    ofParameterGroup mainGroup;
+    
+    
+    
+		
+};
