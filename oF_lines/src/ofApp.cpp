86ce450ab6f5adc4efe88cacb5458ba8d6f0efc2
Symbat Bekzhigit <sb7971@nyu.edu>
2022-04-06 20:49:06 +0400

    Update ofApp.cpp

--- oF_lines/src/ofApp.cpp
+++ oF_lines/src/ofApp.cpp
@@ -4,25 +4,17 @@
 void ofApp::setup(){
 
     intSliderGroup.setName("Distribution");
-    intSliderGroup.add(intSlider1.set("Lines 1", 50, 0, ofGetWidth()/2));//length of line - x component
-    intSliderGroup.add(intSlider2.set("Lines 2", 50, 0, ofGetWidth()/2));//length of line - y component
-    
-    floatSliderGroup.setName("Thickness");
-    floatSliderGroup.add(floatSlider1.set("Lines 1", 5, 1, 20));//thickness of line
-    floatSliderGroup.add(floatSlider2.set("Lines 1", 5, 1, 20));//thickness of line
-    
+    intSliderGroup.add(intSlider1.set("Lines 1",395, 1, 500));//length of line - x component
+    intSliderGroup.add(intSlider2.set("Lines 2", 301, 1, 500));//length of line - y component
+
     
     params.setName("Colors");
     
-    params.add(color1.set("Lines 1", ofVec3f(100,130,160), ofVec3f(0,0,0), ofVec3f(255,255,255)));
+    params.add(color1.set("Lines 1", ofVec3f(116,96,160), ofVec3f(0,0,0), ofVec3f(255,255,255)));
     
-    params.add(color2.set("Lines 2", ofVec3f(100,130,160), ofVec3f(0,0,0), ofVec3f(255,255,255)));
-    
-    params.add(color3.set("Background", ofVec3f(0,0,0), ofVec3f(0,0,0), ofVec3f(255,255,255)));
- 
+    params.add(color2.set("Lines 2", ofVec3f(140,126,190), ofVec3f(0,0,0), ofVec3f(255,255,255)));
     
     mainGroup.add(intSliderGroup);
-    mainGroup.add(floatSliderGroup);
     mainGroup.add(params);
     gui.setup(mainGroup);//insert main group into the gui
     
@@ -30,33 +22,51 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    
+   
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     
-    ofBackground(color3->x, color3->y, color3->z);
+    ofBackground(0,0,0);
     
-    ofSetColor(color1->x,color1->y,color1->z);
-    ofSetLineWidth(floatSlider1);
+   ofSetColor(color1->x,color1->y,color1->z);
+
     
-    ofDrawLine(0,ofGetHeight()/2,ofGetWidth(),ofGetHeight()/2);
-    ofDrawLine(0,ofGetHeight()/2-intSlider1,ofGetWidth(),ofGetHeight()/2-intSlider1);
-    ofDrawLine(0,ofGetHeight()/2+intSlider1,ofGetWidth(),ofGetHeight()/2+intSlider1);
-    ofDrawLine(0,ofGetHeight()/2-2*intSlider1,ofGetWidth(),ofGetHeight()/2-2*intSlider1);
-    ofDrawLine(0,ofGetHeight()/2+2*intSlider1,ofGetWidth(),ofGetHeight()/2+2*intSlider1);
+    for(int i=0; i<intSlider1; i+=20){
+        ofDrawLine(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()/2+i, ofGetHeight()/2-300);
+        ofDrawLine(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()/2-i, ofGetHeight()/2-300);
+        ofDrawLine(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()/2+i, ofGetHeight()/2+300);
+        ofDrawLine(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()/2-i, ofGetHeight()/2+300);
+    }
+ 
+    ofSetColor(color2->x,color2->y,color2->z);
+  
+        
+    for(int i=0; i<intSlider2; i+=20){
+        ofDrawLine(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()/2-400, ofGetHeight()/2+i);
+        ofDrawLine(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()/2-400, ofGetHeight()/2-i);
+        ofDrawLine(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()/2+400, ofGetHeight()/2+i);
+        ofDrawLine(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()/2+400, ofGetHeight()/2-i);
+    }
+   
     
+//    ofDrawLine(0,ofGetHeight()/2,ofGetWidth(),ofGetHeight()/2);
+//    ofDrawLine(0,ofGetHeight()/2-intSlider1,ofGetWidth(),ofGetHeight()/2-intSlider1);
+//    ofDrawLine(0,ofGetHeight()/2+intSlider1,ofGetWidth(),ofGetHeight()/2+intSlider1);
+//    ofDrawLine(0,ofGetHeight()/2-2*intSlider1,ofGetWidth(),ofGetHeight()/2-2*intSlider1);
+//    ofDrawLine(0,ofGetHeight()/2+2*intSlider1,ofGetWidth(),ofGetHeight()/2+2*intSlider1);
+//
+//    ofSetColor(color2->x, color2->y, color2->z);
+//    ofSetLineWidth(floatSlider2);
+//
+//    ofDrawLine(0,ofGetHeight(),ofGetWidth(),0);
+//    ofDrawLine(0,ofGetHeight()-intSlider2,ofGetWidth()-intSlider2,0);
+//    ofDrawLine(0,ofGetHeight()-2*intSlider2,ofGetWidth()-2*intSlider2,0);
+//    ofDrawLine(0+intSlider2,ofGetHeight(),ofGetWidth(),0+intSlider2);
+//    ofDrawLine(0+2*intSlider2,ofGetHeight(),ofGetWidth(),0+2*intSlider2);
     
-    ofSetColor(color2->x, color2->y, color2->z);
-    ofSetLineWidth(floatSlider2);
-    
-    ofDrawLine(0,ofGetHeight(),ofGetWidth(),0);
-    ofDrawLine(0,ofGetHeight()-intSlider2,ofGetWidth()-intSlider2,0);
-    ofDrawLine(0,ofGetHeight()-2*intSlider2,ofGetWidth()-2*intSlider2,0);
-    ofDrawLine(0+intSlider2,ofGetHeight(),ofGetWidth(),0+intSlider2);
-    ofDrawLine(0+2*intSlider2,ofGetHeight(),ofGetWidth(),0+2*intSlider2);
-    
+
     gui.draw();
 }
 
