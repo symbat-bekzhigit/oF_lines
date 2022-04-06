84350cc70609d9b26e043692419ad39d7db8a9a8
Symbat Bekzhigit <sb7971@nyu.edu>
2022-04-06 14:23:18 +0400

    Update Circle.cpp

--- oF_lines/src/Circle.cpp
+++ oF_lines/src/Circle.cpp
@@ -12,6 +12,8 @@ void Circle::setup(){
     params.add(x.set("x", ofGetWidth()/2, 0, ofGetWidth()));
     params.add(y.set("y", ofGetHeight()/2, 0, ofGetHeight()));
     
-    params.add(colors.set("colors", ofVec3f(100,130,160), ofVec3f(0,0,0), ofVec3f(255,255,255)));
+    params.add(color1.set("color1", ofVec3f(100,130,160), ofVec3f(0,0,0), ofVec3f(255,255,255)));
+    
+    params.add(color2.set("color2", ofVec3f(100,130,160), ofVec3f(0,0,0), ofVec3f(255,255,255)));
     
 }
