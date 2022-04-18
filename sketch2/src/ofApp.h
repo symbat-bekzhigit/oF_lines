40c8e8eb3c386f107be5e9c3bb99ff68f987ac7f
Symbat Bekzhigit <sb7971@nyu.edu>
2022-04-18 12:47:58 +0400

    Update ofApp.h

--- sketch2/src/ofApp.h
+++ sketch2/src/ofApp.h
@@ -20,5 +20,42 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+//    ofPoint position;
+//    float radius;
+//    ofColor circleColor;
+//    ofColor drawColor;
+    
+//    float position;
+//    float x;
+//    float y;
+//    float c1;
+//    float c2;
+//    float c3;
+    
+    float angle;
+    float angle2;
+    
+    float rotationSpeed;
+    float rotationSpeed2;
+    int width;
+    int widthLarge;
+    
+    float x, x1, x2, x3, x4;
+    float y, y1, y2, y3, y4;
+    
+    int i;
+    int k;
+    glm::vec2 point;
+    glm::vec2 point2;
+    glm::vec2 point3;
+    glm::vec2 point4;
+    
+    glm::vec2 point_2;
+    glm::vec2 point2_2;
+    glm::vec2 point3_2;
+    glm::vec2 point4_2;
+    
+ 
 		
 };
