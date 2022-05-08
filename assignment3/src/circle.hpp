c1bce503cfc48230ac6be48336f249e8da37d829
Symbat Bekzhigit <sb7971@nyu.edu>
2022-05-08 17:40:05 +0400

    Create circle.hpp

---
+++ assignment3/src/circle.hpp
@@ -0,0 +1,22 @@
+//
+//  circle.hpp
+//  assignment3
+//
+//  Created by Symbat Bekzhigit on 03/05/2022.
+//
+
+#ifndef circle_hpp
+#define circle_hpp
+
+#include "ofMain.h"
+
+class Circle {
+public:
+    int x;
+    int y;
+    int radius;
+    void setup();
+};
+
+
+#endif /* circle_hpp */
