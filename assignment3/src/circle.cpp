9b4780504af46477830f981705c7caf854d26a01
Symbat Bekzhigit <sb7971@nyu.edu>
2022-05-08 17:40:04 +0400

    Create circle.cpp

---
+++ assignment3/src/circle.cpp
@@ -0,0 +1,19 @@
+//
+//  circle.cpp
+//  assignment3
+//
+//  Created by Symbat Bekzhigit on 03/05/2022.
+//
+
+#include "circle.hpp"
+
+void Circle::setup(){
+//    params.setName("circle params");
+//    params.add(x.set("x", ofGetWidth()/2, 0, ofGetWidth()));
+//    params.add(y.set("y", ofGetHeight()/2, 0, ofGetHeight()));
+//
+//    params.add(colors.set("colors", ofVec3f(100,130,160), ofVec3f(0,0,0), ofVec3f(255,255,255)));
+//
+    
+    
+}
