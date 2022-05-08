fed3e9b79b84ef84569f99a772d4f20fd4bf7eaa
Symbat Bekzhigit <sb7971@nyu.edu>
2022-05-08 18:15:02 +0400

    Update circle.cpp

--- assignment3/src/circle.cpp
+++ assignment3/src/circle.cpp
@@ -7,13 +7,14 @@
 
 #include "circle.hpp"
 
-void Circle::setup(){
+void Circle::setup(int x, int y, int r){
 //    params.setName("circle params");
 //    params.add(x.set("x", ofGetWidth()/2, 0, ofGetWidth()));
 //    params.add(y.set("y", ofGetHeight()/2, 0, ofGetHeight()));
 //
 //    params.add(colors.set("colors", ofVec3f(100,130,160), ofVec3f(0,0,0), ofVec3f(255,255,255)));
-//
-    
+    this->x = x;
+    this->y = y;
+    this->radius = r;
     
 }
